6eafddd85843ddb7cddaf8ff8547dcdb78da2953
scvit <88419463+scvit@users.noreply.github.com>
2024-06-26 13:59:06 +0900

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.55.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "2.52.0"
+    }
+     azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.109.0"
+    }
+     azurestack = {
+      source = "hashicorp/azurestack"
+      version = "1.0.0"
+    }
+     google = {
+      source = "hashicorp/google"
+      version = "5.35.0"
+    }
+     http = {
+      source = "hashicorp/http"
+      version = "3.4.3"
+    }
+     random = {
+      source = "hashicorp/random"
+      version = "3.6.2"
+    }
+     template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "0.56.0"
+    }
+    time = {
+      source = "hashicorp/time"
+      version = "0.11.2"
+    }
+    github = {
+      source = "integrations/github"
+      version = "6.2.2"
+    }
+    oci = {
+      source = "oracle/oci"
+      version = "5.46.0"
+    } 
+    
+  }
+  }
+
+
+
+
+  
